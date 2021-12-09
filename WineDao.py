d0e47f459d3ceda46e85e6ebf9c0eeaa24c8b8b9
Caoimhinv <caoimhinvallely@gmail.com>
2021-12-09 10:03:27 +0000

    few errors fixed and reorganising

--- WineDao.py
+++ WineDao.py
@@ -1,7 +1,3 @@
-
-# other version with pooling in different folder. 
-# Still not saving updates for some reason?
-
 import mysql.connector
 from mysql.connector import cursor
 import dbConfig as cfg
@@ -18,14 +14,11 @@ class WineDao:
 
     def create(self, wine):
         cursor = self.db.cursor()
-        sql = "insert into wines2 (name, producer, vintage, country_region, grape_style) values (%s,%s,%s,%s,%s)"
+        sql = "insert into wines3 (nameProducer, vintage, regionCountry) values (%s, %s, %s)"
         values = [
-            # wine['ID'],
-            wine['name'],
-            wine['producer'],
+            wine['nameProducer'],
             wine['vintage'],
-            wine['country_region'],
-            wine['grape_style']
+            wine['regionCountry'],
         ]
         cursor.execute(sql, values)
         self.db.commit()
@@ -33,55 +26,49 @@ class WineDao:
 
     def getAll(self):
         cursor = self.db.cursor()
-        sql = 'select * from wines2'
+        sql = 'select * from wines3'
         cursor.execute(sql)
         results = cursor.fetchall()
         returnArray = []
-        #print(results)
         for result in results:
             resultAsDict = self.convertToDict(result)
             returnArray.append(resultAsDict)
-
         return returnArray
 
     def findById(self, ID):
         cursor = self.db.cursor()
-        sql = 'select * from wines2 where ID = %s'
+        sql = 'select * from wines3 where ID = %s'
         values = [ ID ]
         cursor.execute(sql, values)
         result = cursor.fetchone()
         return self.convertToDict(result)
         
-
     def update(self, wine):
-       cursor = self.db.cursor()
-       sql = "update wines2 set name = %s, producer = %s, vintage = %s,  country_region = %s, grape_style = %s where ID = %s"
-       values = [
-            wine['name'],
-            wine['producer'],
+        cursor = self.db.cursor()
+        sql = "update wines3 set nameProducer = %s, vintage = %s, regionCountry = %s where ID = %s"
+        values = [
+            wine['nameProducer'],
             wine['vintage'],
-            wine['country_region'],
-            wine['grape_style'],
+            wine['regionCountry'],
             wine['ID'],
         ]
-       cursor.execute(sql, values)
-       self.db.commit()
-       return wine
+        cursor.execute(sql, values)
+        self.db.commit()
+        return wine
 
     def delete(self, ID):
-       cursor = self.db.cursor()
-       sql = 'delete from wines2 where ID = %s'
-       values = [ID]
-       cursor.execute(sql, values)
-    #    self.db.commit()
-       return {}
+        cursor = self.db.cursor()
+        sql = 'delete from wines3 where ID = %s'
+        values = [ ID ]
+        cursor.execute(sql, values)
+        self.db.commit()
+        return {}
 
     def convertToDict(self, result):
-        colnames = ['ID','name', 'producer', 'vintage', 'country_region', 'grape_style']
+        colnames = ['ID', 'nameProducer', 'vintage', 'regionCountry']
         wine = {}
-
         if result:
-            for i , colName in enumerate(colnames):
+            for i, colName in enumerate(colnames):
                 value = result[i]
                 wine[colName] = value
         return wine
--- server.py
+++ server.py
@@ -3,80 +3,56 @@ from WineDao import wineDao
 
 app = Flask(__name__, static_url_path='', static_folder='static_pages')
 
-
 @app.route('/')
 def index():
     return "hello"
+
 #get all
-
-
 @app.route('/wines')
 def getAll():
     return jsonify(wineDao.getAll())
+
 # find By id
-
-
 @app.route('/wines/<int:ID>')
 def findById(ID):
     return jsonify(wineDao.findById(ID))
 
 # create
-# curl -X POST -d "{\"Title\":\"test\", \"Author\":\"some guy\", \"Price\":123}" http://127.0.0.1:5000/wines
-
-
 @app.route('/wines', methods=['POST'])
 def create():
-   
     if not request.json:
         abort(400)
 
     wine = {
-        # "ID": request.json["ID"],
-        "name": request.json["name"],
-        "producer": request.json["producer"],
+        "nameProducer": request.json["nameProducer"],
         "vintage": request.json["vintage"],
-        "country_region": request.json["country_region"],
-        "grape_style": request.json["grape_style"]
+        "regionCountry": request.json["regionCountry"],
     }
     return jsonify(wineDao.create(wine))
-
     return "served by Create "
 
-#update
-# curl -X PUT -d "{\"Title\":\"new Title\", \"Price\":999}" -H "content-type:application/json" http://127.0.0.1:5000/wines/1
-
-
+# update
 @app.route('/wines/<int:ID>', methods=['PUT'])
 def update(ID):
-    foundWine=wineDao.findById(ID)
+    foundWine = wineDao.findById(ID)
     print (foundWine)
     if foundWine == {}:
         return jsonify({}), 404
     currentWine = foundWine
-    if 'name' in request.json:
-        currentWine['name'] = request.json['name']
-    if 'producer' in request.json:
-        currentWine['producer'] = request.json['producer']
+    if 'nameProducer' in request.json:
+        currentWine['nameProducer'] = request.json['nameProducer']
     if 'vintage' in request.json:
         currentWine['vintage'] = request.json['vintage']
-    if 'country/region' in request.json:
-        currentWine['country_region'] = request.json['country_region']
-    if 'grape/style' in request.json:
-        currentWine['grape_style'] = request.json['grape_style']
+    if 'regionCountry' in request.json:
+        currentWine['regionCountry'] = request.json['regionCountry']
     wineDao.update(currentWine)
-
     return jsonify(currentWine)
 
 #delete
-# curl -X DELETE http://127.0.0.1:5000/wines/1
-
-
 @app.route('/wines/<int:ID>', methods=['DELETE'])
 def delete(ID):
     wineDao.delete(ID)
-
     return jsonify({"done": True})
-
 
 if __name__ == "__main__":
     app.run(debug=True)
